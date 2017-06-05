74258cc23b9312640fe9f891a63e0a6afecd5715
Dim <skyuphigh01@gmail.com>
2017-06-05 00:09:50 -0400

    External files

--- src/js/global.js
+++ src/js/global.js
@@ -1,5 +1,8 @@
-(function(){
-    document.oncontextmenu = function(){
-        return false;
-    }
-}());
+$(function(){
+    $.getJSON('projects.json', function(data){
+        for (var i=0;i<data.length;i++) {
+            var arrary = data[i];
+            $('.projects').append('<div class="name">'+arrary.name+'</div><div class="github">'+arrary.github+'</div><div class="demo"><a href="'+arrary.demo+'">Demo</a></div>')
+        }
+    });
+});
