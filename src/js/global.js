b19c95dee000eb67257a99a09e10b051a40e0940
Dim <skyuphigh01@gmail.com>
2017-06-28 17:51:29 -0400

    Update global.js

--- src/js/global.js
+++ src/js/global.js
@@ -1,26 +1,5 @@
-var data = [
-    {
-        "name": "BookmarkletJS",
-        "github": "https://github.com/DimidCO/BookmarkletJS",
-        "demo": "/BookmarkletJS"
-    },
-    {
-        "name": "Fake",
-        "github": "https://github.com/DimidCO/Sample",
-        "demo": "/Fake"
-    },
-    {
-        "name": "Sample",
-        "github": "https://github.com/DimidCO/Sample",
-        "demo": "/Sample"
+(function(){
+    document.oncontextmenu = function(){
+        return false;
     }
-];
-
-$(function(){
-//    $.getJSON('projects.json', function(data){
-        for (var i=0;i<data.length;i++) {
-            var arrary = data[i];
-            $('.projects').append('<name>'+arrary.name+'</name><a href="'+arrary.demo+'" target="_blank">Demo</a><github>'+arrary.github+'</github>')
-        }
-//    });
-});
+}());
