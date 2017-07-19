16db853a9658ecadb4009eb657600874d820bfa6
Dim <skyuphigh01@gmail.com>
2017-07-19 16:58:23 -0400

    +

--- src/js/global.js
+++ src/js/global.js
@@ -1,5 +1,8 @@
 (function(){
-    document.oncontextmenu = function(){
-        return false;
-    }
+  document.oncontextmenu = function(){
+    return false;
+  }
+  window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+  ga('create', 'UA-102905191-1', 'auto');
+  ga('send', 'pageview');
 }());
