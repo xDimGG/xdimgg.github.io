928c80f1cf9355481b1bcad926cc52fc73703430
Dim <skyuphigh01@gmail.com>
2017-07-20 21:31:38 -0400

    +

---
+++ remove-youtube-end-cards/endcard.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         Remove YouTube End Cards
+// @version      1
+// @description  Removes the end cards of YouTube videos
+// @author       Dim
+// @match        *://www.youtube.com/*
+// @require      https://code.jquery.com/jquery-3.2.1.min.js
+// @updateURL    https://dim.dimid.co/remove-youtube-end-cards/endcard.user.js
+// @run-at       document-start
+// @grant        GM_addStyle
+// ==/UserScript==
+
+GM_addStyle('.remove_card{border:1px solid #CCC;background-color:#FAFAFA;color:#737373;cursor:pointer;font-family:"Roboto";padding:4px 8px;border-radius:2px;margin-left:10px;vertical-align:middle;font-size:12px;}');
+
+function disableSPF() {
+	if (unsafeWindow._spf_state && unsafeWindow._spf_state.config) {
+		unsafeWindow._spf_state.config['navigate-limit'] = 0;
+		unsafeWindow._spf_state.config['navigate-part-received-callback'] = function (targetUrl) { location.href = targetUrl; };
+	}
+	setTimeout(disableSPF, 50);
+}
+
+function RemoveCard() {
+    $("#watch7-subscription-container").append($('<span class=remove_card>Remove End Cards</span>'));
+    $(".remove_card").click(function() {
+        $(".ytp-ce-element").remove();
+        $(".remove_card").text("End Cards Removed :\)");
+    });
+}
+
+$(function() {
+	RemoveCard();
+    disableSPF();
+});
