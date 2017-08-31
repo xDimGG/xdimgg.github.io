45fe8541621d8e3070969c1d0937794f4ae562d7
Dim <skyuphigh01@gmail.com>
2017-08-31 16:15:20 -0400

    Update endcard.user.js

--- remove-youtube-end-cards/endcard.user.js
+++ remove-youtube-end-cards/endcard.user.js
@@ -12,36 +12,44 @@
 // @grant        unsafeWindow
 // ==/UserScript==
 
-GM_addStyle('#remove_card{border:1px solid #CCC;background-color:#FAFAFA;color:#737373;cursor:pointer;padding:4px 8px;border-radius:2px;margin-left:10px;vertical-align:middle;font-size:12px}');
-
-Element.prototype.remove = function() {
-	this.parentElement.removeChild(this);
-};
-
-NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
-	for(var i = this.length - 1; i >= 0; i--) {
-		if(this[i] && this[i].parentElement) {
-			this[i].parentElement.removeChild(this[i]);
-		}
-	}
-};
-
 function disableSPF() {
-	if (unsafeWindow._spf_state && unsafeWindow._spf_state.config) {
-		unsafeWindow._spf_state.config['navigate-limit'] = 0;
-		unsafeWindow._spf_state.config['navigate-part-received-callback'] = function (targetUrl) { location.href = targetUrl; };
-	}
-	setTimeout(disableSPF, 50);
+  if (unsafeWindow._spf_state && unsafeWindow._spf_state.config) {
+    unsafeWindow._spf_state.config['navigate-limit'] = 0;
+    unsafeWindow._spf_state.config['navigate-part-received-callback'] = function (targetUrl) { location.href = targetUrl; };
+  }
+  setTimeout(disableSPF, 50);
+}
+
+function installUnsafewindowPolyfill() {
+  if (typeof unsafeWindow === 'undefined') {
+    if (typeof XPCNativeWrapper === 'function' && typeof XPCNativeWrapper.unwrap === 'function') unsafeWindow = XPCNativeWrapper.unwrap(window);
+    else if (window.wrappedJSObject) unsafeWindow = window.wrappedJSObject;
+  }
 }
 
 function removeCard() {
-  document.getElementById('watch7-subscription-container').innerHTML += '<span id=remove_card>Remove End Cards</span>';
-  document.getElementById('remove_card').onclick = function() {
-    document.getElementsByClassName('ytp-ce-element').remove();
-    document.getElementById('remove_card').innerHTML = 'End Cards Removed';
-  };
+  setTimeout(function() {
+    var endCardRemover = document.createElement('span');
+    endCardRemover.appendChild(document.createTextNode('Remove End Cards'));
+    endCardRemover.id = 'remove_card';
+    var sub = document.getElementsByTagName('ytd-subscribe-button-renderer')[0];
+    if (sub) {
+        GM_addStyle('#remove_card{margin:auto 4px;font-weight:500;text-transform:uppercase;letter-spacing:.007px;;background-color:hsl(0, 0%, 93.3%);color:hsla(0, 0%, 6.7%, .6);cursor:pointer;padding:10px 16px;border-radius:2px;vertical-align:middle;font-size:1.4rem}');
+        sub.insertBefore(endCardRemover, sub.firstChild);
+    } else {
+        GM_addStyle('#remove_card{border:1px solid #CCC;background-color:#FAFAFA;color:#737373;cursor:pointer;padding:4px 8px;border-radius:2px;margin-left:10px;vertical-align:middle;font-size:12px}');
+        document.getElementById('watch7-subscription-container').appendChild(endCardRemover);
+    }
+    endCardRemover.onclick = function() {
+        var cards = document.getElementsByClassName('ytp-ce-element');
+        while(cards[0]) {
+            cards[0].parentNode.removeChild(cards[0]);
+        }
+        endCardRemover.innerText = 'End Cards Removed';
+    };
+  }, 1000);
 }
 
 disableSPF();
-
+installUnsafewindowPolyfill();
 document.addEventListener('DOMContentLoaded', removeCard);
